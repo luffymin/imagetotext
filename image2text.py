bb055e12660642086918616a68de560562fd696b
wangjianzheng <wangjianzheng@xhqb.com>
2023-04-19 14:26:27 +0800

    update: rename file

---
+++ image2text.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from PIL import Image
+import pytesseract
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("Usage: " + sys.argv[0] + " <image-file>")
+        sys.exit(0)
+    
+    image_file = sys.argv[1]
+    text = pytesseract.image_to_string(Image.open(image_file))
+    print(text)
